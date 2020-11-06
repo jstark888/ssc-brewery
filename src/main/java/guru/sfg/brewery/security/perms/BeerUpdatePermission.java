9414fb88cc9a3442e5548aa35c608acc485423f6
Jeff Stark <jstark888@gmail.com>
2020-11-05 19:59:13 -1000

    Created package security.perms Created annotation interfaces related to beer, brewery and customer CRUD authorities Modified BeerRestController, BeerController, BreweryController and CustomerController to replace all @PreAuthorize annotations with the appropriate custom annotation from the security.perms package

---
+++ src/main/java/guru/sfg/brewery/security/perms/BeerUpdatePermission.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.security.perms;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Jeff Stark on 11/5/2020
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('beer.update')")
+public @interface BeerUpdatePermission {
+}
