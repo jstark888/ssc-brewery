d1fa7955a0b034f763643ad83244b2a819712368
Jeff Stark <jstark888@gmail.com>
2020-11-22 13:54:45 -1000

    Modified BeerOrderService added listOrders(Pageable) to interface Modified BeerOrderServiceImpl added implementation of listOrders(Pageable) Added BeerOrderControllerV2 which refactors BeerOrderController to not take the customer id in the path of the request but obtain the customer id from the User retrieved during authentication Added BeerOrderReadPermissionV2 to implement security configuration appropriate to the new BeerOrderControllerV2 Added BeerOrderControllerV2Test to implement tests for the new BeerOrderControllerV2

---
+++ src/main/java/guru/sfg/brewery/security/perms/BeerOrderReadPermissionV2.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.security.perms;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by Jeff Stark on 11/22/2020
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAnyAuthority('order.read', 'customer.order.read')")
+public @interface BeerOrderReadPermissionV2 {
+}
