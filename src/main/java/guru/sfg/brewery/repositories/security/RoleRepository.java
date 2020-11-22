18e56304b8766e0fc6f9d3adf73f8f17f03c4e71
Jeff Stark <jstark888@gmail.com>
2020-11-21 20:40:23 -1000

    Deleted UserDataLoader Modified DefaultBreweryLoader to add functionality that was in deleted UserDataLoader, rename loadCustomerData method to loadTastingRoomData and crate a new loadCustomerData method and createOrder method to create a few beer orders Modified User to add field customer Modified Customer to add field users Modified RoleRepository to add method findByName(String)

--- src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/RoleRepository.java
@@ -3,10 +3,13 @@ package guru.sfg.brewery.repositories.security;
 import guru.sfg.brewery.domain.security.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Jeff Stark on 11/5/2020
  */
 
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String customer);
 }
